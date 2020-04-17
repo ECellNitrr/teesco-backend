793c7ca4d8f31cee4dd823c47b9b8bccf451b441
Sahil Silare <32628578+sahil9001@users.noreply.github.com>
2020-04-17 18:43:49 +0530

    Update server/urls.py
    Co-Authored-By: Naveen Sundar <naveennvrgup@gmail.com>

--- server/urls.py
+++ server/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/query',include('query.urls'))
+    path('api/query/',include('query.urls'))
 ]
