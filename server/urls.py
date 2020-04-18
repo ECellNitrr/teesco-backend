ad15e1499fa1600a9a5b740ee51b73cbdbcddb41
sahil9001 <sahilsilare@gmail.com>
2020-04-18 14:09:51 +0530

    Changes to be committed: 	modified:   server/urls.py 	new file:   users/__init__.py 	new file:   users/admin.py 	new file:   users/apps.py 	new file:   users/migrations/__init__.py 	new file:   users/models.py 	new file:   users/tests.py 	new file:   users/urls.py 	new file:   users/views.py

--- server/urls.py
+++ server/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/query/',include('query.urls'))
+    path('api/query/',include('query.urls')),
+    path('api/users/',include('users.urls'))
 ]
