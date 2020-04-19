3f147bffe90e3566d0408fddaf90041fcde63a26
priyanshisharma <priyanshi417@gmail.com>
2020-04-19 17:48:17 +0530

    added org

--- org/urls.py
+++ org/urls.py
@@ -2,8 +2,5 @@ from django.contrib import admin
 from django.urls import path, include 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/query/',include('query.urls')),
-    path('api/users/',include('users.urls')),
-    path('api/org/',include('org.urls')),
+
 ]
