a58c8b5c570b4bd4fc8d2960c911879aa45e5d13
priyanshisharma <priyanshi417@gmail.com>
2020-04-20 20:59:16 +0530

    User model registered

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from users.models import User, Notification
 
 
 # Register your models here.
-admin.site.register(User, UserAdmin)
+admin.site.register(User)
 admin.site.register(Notification)
