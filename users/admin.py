e3216797d10f2fd000afd364c56a760ba902b9da
priyanshisharma <priyanshi417@gmail.com>
2020-04-20 17:59:00 +0530

    Added user models

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from users.models import User, Notification
+
 
 # Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(Notification)
