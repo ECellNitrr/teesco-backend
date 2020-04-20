180d6639a1fa1c19ae2f59ba6130fb2cd6287fb9
Naveen Sundar <naveennvrgup@gmail.com>
2020-04-21 05:14:07 +0530

    feat: Add PermissionSetField class

--- org/custom_model_field.py
+++ org/custom_model_field.py
@@ -3,6 +3,9 @@ from django.db import models
 
 
 class PermissionSet:
+    """Helps in interconversion of permissions into objects(for ease of use) and strings(for storage in db)."""
+
+    # These mappings are to reduce the chance to misspell the permission name
     IS_ADMIN = 'IS_ADMIN'
     IS_STAFF = 'IS_STAFF'
     CAN_CREATE_TASKS = 'CAN_CREATE_TASKS'
@@ -11,6 +14,8 @@ class PermissionSet:
 
 
     def __init__(self, permission_string=''):
+        """Creates a permission set object from a list of comma separated permission names string."""
+        
         self.permissions = defaultdict(bool)
         permissions_array = permission_string.split(',')
 
@@ -20,12 +25,16 @@ class PermissionSet:
 
 
     def set_permissions(self, permissions_array):
+        """Revokes all the permissions and sets the permissions supplied in the array."""
+
         self.permissions = defaultdict(bool)
         for permission in permissions_array:
             self.permissions[permission]=True
 
 
     def stringify(self):
+        """Converts the permissions that are set(True) into string to be stored in the database."""
+
         permissions_array = []
 
         if self.permissions[self.IS_ADMIN]:
@@ -45,3 +54,22 @@ class PermissionSet:
 
         permission_string = ','.join(permissions_array)
         return permission_string
+
+
+class PermissionSetField(models.CharField):
+    """
+    Custom model field to store permissions. Automatically converts db string to permission object
+    and permission object into string to be stored in the database.
+    """
+
+    max_length = 8192
+
+    def to_python(self, value):
+        """Converts string from db into PermissionSet object"""
+
+        return PermissionSet(value)
+
+    def get_prep_value(self, value):
+        """Converts PermissionSet object into string to store in db"""
+
+        return value.stringify()
