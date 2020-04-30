c0d6a43bed941f0335c69675475801e6ea252d23
Ankur Kunal <mrankur801@gmail.com>
2020-04-30 17:43:25 +0530

    removed all the errors related to PR

--- server/urls.py
+++ server/urls.py
@@ -20,15 +20,15 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 
-# for testing
+
 schema_view = get_schema_view(
    openapi.Info(
       title="teesco-API",
       default_version='v1',
       description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
+      terms_of_service="https://https://ecell.nitrr.ac.in/terms",
+      contact=openapi.Contact(email="ecellnitrr.opensource@gmail.com"),
+      license=openapi.License(name="Apache License 2.0"),
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
@@ -40,9 +40,6 @@ urlpatterns = [
     path('api/users/',include('users.urls')),
     path('api/org/',include('org.urls')),
     path('api/tasks/',include('tasks.urls')),
-#     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-#     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-#     url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
-    path('swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
-    # path('redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
+
 ]
