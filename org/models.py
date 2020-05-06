7b0ae5ee546587e45c9407b9273f835bb70a5c6e
priyanshisharma <priyanshi417@gmail.com>
2020-05-07 02:19:56 +0530

    Models enhanced

--- org/models.py
+++ org/models.py
@@ -1,24 +1,27 @@
 from django.db import models
-from django.utils import timezone
 from .custom_model_field import *
 from users.models import User
 
 # Create your models here.
 
+class PermissionSetTestModel(models.Model):
+    perm = PermissionField()
+
 class Org (models.Model):
-    route_slug = models.SlugField(max_length = 40)
+    route_slug = models.SlugField(max_length = 40, unique=True)
     can_join_without_invite = models.BooleanField(default = True)
     name = models.CharField(max_length=30)
     tagline = models.CharField(max_length = 50)
-    about = models.CharField(max_length = 500)
-    profile_pic = models.ImageField(upload_to = 'teesco-backend/static/profile_pic')
-    cover_pic = models.ImageField(upload_to = 'teesco-backend/static/cover_pic')
-    created_at = models.DateTimeField(default = timezone.now)
+    about = models.CharField(max_length = 500, null=True)
+    profile_pic = models.ImageField(upload_to = 'uploads/org/profile_pic', null=True)
+    cover_pic = models.ImageField(upload_to = 'uploads/org/cover_pic', null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
 
 class PermissionSet(models.Model):
+    name = models.CharField(max_length=30)
     org = models.ForeignKey(Org,on_delete=models.CASCADE)
     permissions = PermissionField()
 
@@ -30,7 +33,7 @@ class PermissionSet(models.Model):
 class Group (models.Model):
     name = models.CharField(max_length = 30)
     role = models.CharField(max_length = 200)
-    invite_slug = models.SlugField(max_length = 40)
+    invite_slug = models.SlugField(max_length = 40, unique=True)
     org = models.ForeignKey(Org,on_delete=models.CASCADE)
     default_permission_set  = models.ForeignKey(PermissionSet, on_delete=models.CASCADE)
 
@@ -45,7 +48,7 @@ class Member (models.Model):
     permissions  = models.ForeignKey(PermissionSet, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.user.name + self.id
+        return f'{self.id}-{self.org.name}-{self.group.name}-{self.user.name}'
 
 
 class Leaderboard (models.Model):
@@ -54,4 +57,4 @@ class Leaderboard (models.Model):
     org = models.ForeignKey(Org,on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.user.name + self.id
+        return f'{self.id}-{self.org.name}-{self.user.name}-{self.points}'
