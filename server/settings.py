3d8ae1118cfba6ad8e59049ad39e4fe6195542a9
sahil9001 <sahilsilare@gmail.com>
2020-05-07 11:29:54 +0530

    Changes to be committed: 	modified:   server/settings.py

--- server/settings.py
+++ server/settings.py
@@ -28,7 +28,7 @@ CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'India/Delhi'
+CELERY_TIMEZONE = 'Asia/Kolkata'
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
@@ -51,8 +51,7 @@ INSTALLED_APPS = [
     'query',
     'users',
     'org',
-    'tasks',
-    'core'
+    'tasks'
 ]
 
 MIDDLEWARE = [
