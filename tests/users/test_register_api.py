6c289d3055dfdcd553fb41707c3a77701265dd61
priyanshisharma <priyanshi417@gmail.com>
2020-05-09 19:12:21 +0530

    Added tests

---
+++ tests/users/test_register_api.py
@@ -0,0 +1,20 @@
+from users.models import User
+from rest_framework.test import APITestCase
+from rest_framework import status
+
+
+class RegistrationTestCase(APITestCase):
+
+    def test_registration(self):
+        data = {"email" : "test@test.test","name" : "test","institution": "testing","country_code": "+00","phone": "0000000000"}
+        response = self.client.post("/api/users/register/", data)
+        
+        self.assertEqual(response.status_code , status.HTTP_201_CREATED)
+        self.assertEqual(response.data["email"] , "test@test.test")
+        self.assertEqual(response.data["username"] , "test@test.test")
+        self.assertEqual(response.data["name"] , "test")
+        self.assertEqual(response.data["institution"] , "testing")
+        self.assertEqual(response.data["country_code"] , "+00")
+        self.assertEqual(response.data["phone"] , "0000000000")
+
+        self.assertFalse(response.data["username"] , "test")
