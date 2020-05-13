fa9a1013514e3dae5343db40a47fa8b7830b1021
Naveen Sundar <naveennvrgup@gmail.com>
2020-05-13 12:42:11 +0530

    docs: Add docs api/users/register/

--- server/settings.py
+++ server/settings.py
@@ -136,3 +136,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# drf-yasg 
+SWAGGER_SETTINGS  = {
+    'DEFAULT_MODEL_RENDERING' : 'example'
+}
--- tests/users/tests_register_api.py
+++ tests/users/tests_register_api.py
@@ -2,12 +2,10 @@ from users.models import User
 from rest_framework.test import APITestCase
 from rest_framework import status
 
-
 class RegistrationTestCase(APITestCase):
 
     def tests_registration(self):
         data = {"email" : "test@test.test","name" : "test","institution": "testing","password": "testingg","country_code": "+00","phone": "0000000000"}
         response = self.client.post("/api/users/register/", data)
         
-        self.assertEqual(response.data["success"] , True)
-        self.assertEqual(response.data["message"] , 'Registration Successful')
+        self.assertEqual(response.status_code , status.HTTP_201_CREATED)
