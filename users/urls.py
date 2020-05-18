a7164ec1e75ba4827df02bf89b3b28701143d50a
anshsrtv <49693160+anshsrtv@users.noreply.github.com>
2020-05-18 09:32:50 +0530

    feat: profile api  (#82)

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include 
+from django.urls import path, include
 from users.views import *
 
 urlpatterns = [
-    path('register/', RegistrationView.as_view(), name = 'register'),
-    path('login/', LoginView.as_view(), name = 'login'),
+    path('', profile_view, name='profile'),
+    path('register/', RegistrationView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
 ]
