c36c7a3b511ef275715723fa08f59d78dcbd4a8f
Ankur Kunal <mrankur801@gmail.com>
2020-05-19 17:32:06 +0530

    final update

--- users/models.py
+++ users/models.py
@@ -6,16 +6,6 @@ from django.contrib.auth.models import AbstractUser
 class User (AbstractUser):
     route_slug = models.SlugField(max_length=40)
     email = models.EmailField(max_length=50)
-<<<<<<< HEAD
-    name = models.CharField(max_length=30)
-    institution = models.CharField(max_length=30)
-    country_code = models.CharField(max_length=6)
-    phone = models.CharField(max_length=10)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name
-=======
     name = models.CharField(max_length=30, null=False, blank=False)
     password = models.CharField(max_length = 30)
     username = models.CharField(max_length=30, null=False, blank=False, unique = True)
@@ -26,7 +16,6 @@ class User (AbstractUser):
 
     def __str__(self):
         return self.username
->>>>>>> upstream/dev
 
 class Notification (models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
