bef9d18137f9794c8c017f0624e3af0d9a6a981e
anshsrtv <ansh_srtv@outlook.com>
2020-05-19 13:59:57 +0530

    fix: reverted unwanted commits

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('', profile_view, name='profile'),
     path('register/', RegistrationView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
-    path('org/', list_orgs_view, name='list_orgs'),
 ]
--- users/views.py
+++ users/views.py
@@ -11,7 +11,6 @@ from utils.swagger import set_example
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
-from org.models import *
 
 
 class RegistrationView(APIView):
@@ -90,35 +89,3 @@ def profile_view(request):
         'created_at': user.created_at
     }
     return Response(user_object, status.HTTP_200_OK)
-
-
-@swagger_auto_schema(
-    operation_id="list_orgs",
-    method='get',
-    responses={
-        '200': set_example(responses.list_orgs_200),
-        '401': set_example({"detail": "Authentication credentials were not provided."})
-    }
-)
-@api_view(['get'])
-@permission_classes([IsAuthenticated])
-def list_orgs_view(request):
-    """
-    1. The API lists all the orgs the authorized user is a part of. 
-    2. The API gives information about the orgs listed and 
-    the group (user_role) the user is in. 
-    3. It returns a null for profile pic if no picture url is found in that field.
-    """
-    members = Member.objects.filter(user=request.user)
-    response_object = []
-    for member in members:
-        org = {
-            'id': member.org.id,
-            'org_name': member.org.name,
-            'user_role': member.group.name,
-            'profile_pic': member.org.profile_pic if member.org.profile_pic else "null",
-            'route_slug': member.org.route_slug,
-            'tagline': member.org.tagline
-        }
-        response_object.append(org)
-    return Response(response_object, status.HTTP_200_OK)
