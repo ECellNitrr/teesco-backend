b1abd147acb04fe447869b29f22d2b2b6cac5f67
Ankur Kunal <mrankur801@gmail.com>
2020-05-21 15:12:02 +0530

    removed all commneted line

--- server/urls.py
+++ server/urls.py
@@ -18,27 +18,6 @@ schema_view = get_schema_view(
 )
 
 
-
-# from rest_framework import permissions
-# from drf_yasg.views import get_schema_view
-# from drf_yasg import openapi
-
-
-# # for testing
-# schema_view = get_schema_view(
-#    openapi.Info(
-#       title="teesco-API",
-#       default_version='v1',
-#       description="Test description",
-#       terms_of_service="https://www.google.com/policies/terms/",
-#       contact=openapi.Contact(email="contact@snippets.local"),
-#       license=openapi.License(name="BSD License"),
-#    ),
-#    public=True,
-#    permission_classes=(permissions.AllowAny,),
-# )
-
-
 urlpatterns = [
     path('docs/',schema_view.with_ui('swagger',cache_timeout=0), name = 'schema-swagger-ui'),
     path('admin/', admin.site.urls),
