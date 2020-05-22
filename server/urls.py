977927ab6d86488adbfd77858be6a09bbeb71966
KunalAnkur <57923976+KunalAnkur@users.noreply.github.com>
2020-05-22 22:18:04 +0530

    Error messages for unregistered users in login api (#89)
    * added task org and users
    
    * updated requirements.txt
    
    * added swagger
    
    * testing swagger doc
    
    * updated requirements.txt
    
    * original
    
    * changes the title of swagger doc
    
    * removing all syntax error
    
    * updated the repository
    
    * final update
    
    * remove an error
    
    * solved the issue 87
    
    * removed all the test errors
    
    * removed a final message
    
    * removed all commneted line
    
    * updated testsLoginApi.py file
    
    * resolving again hope it is final
    
    Co-authored-by: Ankur Kunal <mrankur@gmail.com>

--- server/urls.py
+++ server/urls.py
@@ -17,6 +17,7 @@ schema_view = get_schema_view(
    permission_classes=(permissions.AllowAny,),
 )
 
+
 urlpatterns = [
     path('docs/',schema_view.with_ui('swagger',cache_timeout=0), name = 'schema-swagger-ui'),
     path('admin/', admin.site.urls),
