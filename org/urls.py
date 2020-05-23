a336ff92a38b6e620fbad68d6a014b09b09ab371
priyanshisharma <priyanshi417@gmail.com>
2020-05-24 01:57:31 +0530

    Permission List API Added

--- org/urls.py
+++ org/urls.py
@@ -4,5 +4,6 @@ from .views import *
 
 urlpatterns = [
     path('', OrgView.as_view()),
-    path('<int:org_id>/volunteer/',AddVolunteer,name='add_volunteer')
+    path('<int:org_id>/volunteer/',AddVolunteer,name='add_volunteer'),
+    path('<int:org_id>/permission_set/',Permission_Set_List, name = 'View Permission Set List')
 ]
