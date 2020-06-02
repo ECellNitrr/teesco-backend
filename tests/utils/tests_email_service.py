72377d3cbdd137912ded1c2e35a28d2b7ec2cfb3
sahil9001 <sahilsilare@gmail.com>
2020-06-02 16:31:07 +0530

    feat: Added Mock email service and AWS SES support

---
+++ tests/utils/tests_email_service.py
@@ -0,0 +1,4 @@
+import mock 
+
+def test_send_email():
+    send_email(['sahilsilare@gmail.com'],'TEST','TEST')
--- utils/email_service.py
+++ utils/email_service.py
@@ -1,17 +1,55 @@
 from celery import shared_task
 from decouple import config
-from django.core.mail import EmailMessage
+from django.core.mail import EmailMessage, send_mail
 from decouple import config
+import boto3
 
 
 @shared_task
-def send_email(recipient,subject,body):
-    if config('MOCK_EMAIL', cast=bool, default=True):
+def send_email(recipient, subject, body):
+    if config('MOCK_EMAIL', cast=bool):
         print('Mock email service:')
-        print('Subject:',subject)
-        print('Recipients:',', '.join(recipient))
-        print('Subject:',subject)
+        print('Subject:', subject)
+        print('Recipients:', ', '.join(recipient))
+        print('Subject:', subject)
     else:
-        msg = EmailMessage(subject, body, config('EMAIL_HOST_USER') , recipient)
-        msg.content_subtype = "html"  
-        msg.send()
+        if config('EMAIL_BACKEND', cast=str) == 'django_amazon_ses.EmailBackend':
+            client = boto3.client(
+                'ses',
+                aws_access_key_id=config('AWS_ACCESS_KEY_ID'),
+                aws_secret_access_key=config('AWS_SECRET_ACCESS_KEY'),
+                region_name='eu-west-1'
+            )
+            try:
+                sender_email = config('EMAIL_HOST_USER')
+                print(sender_email)
+                response = client.send_email(
+                    Source=sender_email,
+                    Destination={
+                        'ToAddresses': recipient
+                    },
+                    Message={
+                        'Subject': {
+                            'Data': subject,
+                        },
+                        'Body': {
+                            'Text': {
+                                'Data': body,
+                            },
+                            'Html': {
+                                'Data': '',
+                            }
+                        }
+                    },
+                )
+            except:
+                raise ValueError("Invalid SES credentials.")
+
+        elif config('EMAIL_BACKEND', cast=str) == 'django.core.mail.backends.smtp.EmailBackend':
+            try:
+                msg = EmailMessage(subject, body, config(
+                    'EMAIL_HOST_USER'), recipient)
+                msg.content_subtype = "html"
+                msg.send()
+            except:
+                raise ValueError("Invalid Gmail credentials.")
