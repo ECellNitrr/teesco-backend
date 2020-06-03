f284442fa9ca5ce058b1c041a1587b5f90cde350
Priyanshi Sharma <priyanshi417@gmail.com>
2020-06-03 10:17:49 +0530

    group details api (#111)
    * Added group detail API and tests
    
    * Added group detail tests
    
    * Renamed Tests
    
    * Tests docstring added
    
    * Rectified docstring
    
    * Modified id for respective tests

--- org/urls.py
+++ org/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path('<int:org_id>/', EditOrg, name='edit_org'),
     path('<int:org_id>/volunteer/', AddVolunteer, name='add_volunteer'),
     path('<int:org_id>/group/', GetGroup, name='get_group'),
+    path(
+        '<int:org_id>/group/<int:group_id>/',
+        GroupDetailsView.as_view(),
+        name='get_group_details'
+    ),
 ]
