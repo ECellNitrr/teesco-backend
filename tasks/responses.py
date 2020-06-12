8baee687c2658cab38b80c1215a505fbcc3f575f
Priyanshi Sharma <priyanshi417@gmail.com>
2020-06-12 11:48:48 +0530

    Create task api (#129)
    * Added members list API and profile_pic field
    
    * Added Create Task API and respective tests and changes
    
    * Revert "Added members list API and profile_pic field"
    
    This reverts commit 2ba44f4ce105b685ec89f62953cc81f2bdacf298.
    
    * Changed serializer and added tests
    
    * Added Tests
    
    * Added Tests
    
    * Fixes
    
    * fixes contd.
    
    * Update tasks/serializers.py
    
    Co-authored-by: Naveen Sundar <naveennvrgup@gmail.com>
    
    * Update tasks/serializers.py
    
    Co-authored-by: Naveen Sundar <naveennvrgup@gmail.com>
    
    * Update tasks/serializers.py
    
    Co-authored-by: Naveen Sundar <naveennvrgup@gmail.com>
    
    * linting
    
    * Increased max_length of share_link to 500
    
    * fixes and omit redundance
    
    Co-authored-by: anshsrtv <49693160+anshsrtv@users.noreply.github.com>
    Co-authored-by: Naveen Sundar <naveennvrgup@gmail.com>

---
+++ tasks/responses.py
@@ -0,0 +1,30 @@
+org_not_present_404 = {
+    "detail":"This organisation does not exist"
+}
+
+user_not_present_401 = {
+    "detail":"You are not a member of this organisation"
+}
+
+user_unauthorized_403 = {
+    "detail":"You do not have the required permissions."
+}
+
+create_task_400 = {
+
+    "social_media_platform": [
+        "This field may not be blank."
+    ],
+    "share_type": [
+        "This field may not be blank.",
+    ],
+    "Share_Link":[
+        "Share link is required."
+    ],
+    "Share_text":[
+        "Share text is required."
+    ],
+    "Share_img":[
+        "Share image is required"
+    ]
+}
