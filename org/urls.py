757074f43f1d47b2dbea826f2af0920be7b0f648
Naveen Sundar <naveennvrgup@gmail.com>
2020-06-16 11:20:15 +0530

    feat: Add available permissions api

--- org/urls.py
+++ org/urls.py
@@ -3,11 +3,31 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', OrgView.as_view()),
-    path('<int:org_id>/volunteer/', AddVolunteer, name='add_volunteer'),
-    path('<int:org_id>/group/', GroupView.as_view()),
-    path('<int:org_id>/', EditOrg, name='edit_org'),
-    path('<int:org_id>/profile_pic/', UpdateProfilePic, name='update_profile_pic'),
+    path(
+        '', 
+        OrgView.as_view(),
+        name='org_view'
+    ),
+    path(
+        '<int:org_id>/volunteer/', 
+        AddVolunteer, 
+        name='add_volunteer'
+    ),
+    path(
+        '<int:org_id>/group/', 
+        GroupView.as_view(),
+        name='group_view',
+    ),
+    path(
+        '<int:org_id>/', 
+        EditOrg, 
+        name='edit_org'
+    ),
+    path(
+        '<int:org_id>/profile_pic/', 
+        UpdateProfilePic, 
+        name='update_profile_pic'
+    ),
     path(
         '<int:org_id>/group/<int:group_id>/',
         GroupDetailsView.as_view(),
@@ -18,5 +38,10 @@ urlpatterns = [
         MembersListView.as_view(),
         name='group_members_list'
 
+    ),
+    path(
+        'available_permissions/', 
+        AvailablePermissionsView.as_view(), 
+        name='available_permissions'
     ),
 ]
