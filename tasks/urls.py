7149e6db66ca5ed62c483929767351da5201a320
Akhil Soni <58397226+akhil-maker@users.noreply.github.com>
2021-03-14 13:04:01 +0530

    Adding get_task url

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,4 +3,5 @@ from .views import TaskView
 
 urlpatterns = [
     path('org/<int:org_id>', TaskView.as_view(), name='create_task'),
+    path('org/<int:org_id>', TaskView.as_view(), name='get_task'),
 ]
