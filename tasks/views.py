b92cf98a0aca775167f55bd866164e8c96b8c104
Akhil Soni <58397226+akhil-maker@users.noreply.github.com>
2021-03-14 13:02:54 +0530

    Adding GetTask View

--- tasks/views.py
+++ tasks/views.py
@@ -16,6 +16,53 @@ class TaskView(APIView):
 
     parser_classes = [MultiPartParser]
     permission_classes = [IsAuthenticated]
+    
+    @swagger_auto_schema(
+        operation_id='get_task',
+        responses={
+            '200': set_example(responses.get_task_200),
+            '404': set_example(responses.org_not_present_404),
+            '401': set_example(responses.user_not_present_401),
+            '403': set_example(responses.user_unauthorized_403),
+            '400': set_example(responses.get_task_400)
+        },
+    )
+
+    def get(self, request, org_id):
+        try:
+            org = Org.objects.get(pk=org_id)
+        except Org.DoesNotExist:
+            return Response(responses.org_not_present_404, status.HTTP_404_NOT_FOUND)
+
+        # Check if the user is a member of the organisation
+        try:
+            member = Member.objects.get(
+                user=request.user,
+                org=org
+
+            )
+        except Member.DoesNotExist:
+            return Response(responses.user_not_present_401, status.HTTP_401_UNAUTHORIZED)
+
+        # Check if member is volunteer
+        if member.group.name == 'Volunteer':
+            response_object = []
+            tasks = Task.objects.filter(org=org)
+            for i in tasks:
+                response_body = {
+                    "task_id": i.id,
+                    "author": i.author.username,
+                    "social_media_platform": i.social_media_platform,
+                    "description": i.description,
+                    "share_type": i.share_type,
+                    "share_link": i.share_link,
+                    "share_text": i.share_text,
+                    "share_img": request.build_absolute_uri(i.share_img.url) if i.share_img else None
+                }
+                response_object.append(response_body)
+            return Response(response_object, status.HTTP_200_OK)
+        else:
+            return Response({"detail": "You are not authorised to view this."}, status.HTTP_403_FORBIDDEN)
 
     @swagger_auto_schema(
         operation_id='create_task',
