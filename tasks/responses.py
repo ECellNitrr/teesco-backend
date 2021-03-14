a4d1faaf7439fa87a2c247b9b3642d3d7d419036
Akhil Soni <58397226+akhil-maker@users.noreply.github.com>
2021-03-14 13:06:56 +0530

    Responses for get_task api

--- tasks/responses.py
+++ tasks/responses.py
@@ -28,3 +28,26 @@ create_task_400 = {
         "Share image is required"
     ]
 }
+
+get_task_200 = [
+    {
+        "task_id":1,
+        "author":"Teesco",
+        "social_media_platform":"Whatsapp",
+        "description":"Teesco VMS to manage the volunteers",
+        "share_type":"link",
+        "share_link":"null",
+        "share_text":"texts for teesco",
+        "share_img":"null",
+    },
+    {
+        "task_id":2,
+        "author":"Teesco",
+        "social_media_platform":"Whatsapp",
+        "description":"Teesco VMS to manage the volunteers",
+        "share_type":"link",
+        "share_link":"null",
+        "share_text":"texts for teesco",
+        "share_img":"null",
+    }
+]
